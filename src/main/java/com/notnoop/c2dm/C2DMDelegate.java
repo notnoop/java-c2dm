5016d0ce176967e85b9979a984d96d183c45c374
notnoop <mahmood@notnoop.com>
2011-07-07 20:03:33 -0400

    update delegate documentation

--- src/main/java/com/notnoop/c2dm/C2DMDelegate.java
+++ src/main/java/com/notnoop/c2dm/C2DMDelegate.java
@@ -31,8 +31,44 @@
 package com.notnoop.c2dm;
 
 public interface C2DMDelegate {
+    /**
+     * Delegate called when a notification is sent successfully to the Google
+     * C2DM servers.
+     *
+     * @param message   the sent message (null if client didn't create a
+     *              {@link C2DMNotification} object)
+     * @param response  the response Google sent
+     * @param id    the id assigned to the message by Google servers
+     */
     public void messageSent(C2DMNotification message, C2DMResponse response, String id);
+
+    /**
+     * Delegate method called when the C2DM server rejects or drops a message.
+     *
+     * The server may drop the message for various reasons.  Some may require
+     * the application to retry (e.g. when servers are down or over quota),
+     * and some require more work (e.g. invalid authentication, missing values).
+     *
+     * @param message   the sent message (null if client didn't create a
+     *              {@link C2DMNotification} object)
+     * @param response  the response Google sent
+     */
     public void messageFailed(C2DMNotification message, C2DMResponse response);
 
+    /**
+     * Delegate method called when Google servers update the Auth Token.
+     *
+     * Google servers update the authentication code, and indicates the new
+     * value in special HTTP header "Update-Client-Auth".  Google C2DM servers
+     * may accept the stale value for a unspecified period of time allowing all
+     * clients to be update.
+     *
+     * This library maintains the authentication token automatically, so
+     * subclasses should only use this method for record keeping.  Subclasses
+     * should not override this method to update any {@link C2DMService} or
+     * recreate {@link C2DMService} instances.
+     *
+     * @param newAuthToken  the new authentication token
+     */
     public void authTokenUpdated(String newAuthToken);
 }
